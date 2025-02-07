e4a7d6bccdcf0a9339b865a9112ea5d6ada8180b
GustavoAlmeidaAboud <60568616+GustavoAlmeidaAboud@users.noreply.github.com>
2025-02-07 21:20:41 +0100

    Update functions.ps1

--- functions.ps1
+++ functions.ps1
@@ -1,15 +1,14 @@
-# Check whether Nuget package is installed or not
+function TranscriptVariables() {
+    $date = get-date -format "dddd-MM-dd-HH"
+    $module = "psappdeploytoolkit"
+    $logPath = "C:\ProgramData\Microsoft\IntuneManagementExtension\Logs\install_$module_Powershell_Module_$date.log"
+    }
+    
 function Dependencies {
     if (!(Get-PackageProvider -Name NuGet -ListAvailable -ErrorAction SilentlyContinue)) {
         Write-Host "NuGet package provider is not installed. Installing..." -ForegroundColor Yellow
+        Install-PackageProvider -Name NuGet -Force
         Write-Host "NuGet package provider installed successfully." -ForegroundColor Green
     } else {
         Write-Host "NuGet package provider is already installed." -ForegroundColor Green
     }
-}
-
-function TranscriptVariables{
-    $date = get-date -format "dddd-MM-dd-HH"
-    $module = "psappdeploytoolkit"
-    $logPath = "C:\ProgramData\Microsoft\IntuneManagementExtension\Logs\uninstall_$module_Powershell_Module_$date.log"
-}
