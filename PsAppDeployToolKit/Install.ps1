7f16519972eeaf572d3d0a69531fb69954247891
GustavoAlmeidaAboud <60568616+GustavoAlmeidaAboud@users.noreply.github.com>
2025-02-07 20:48:21 +0100

    Update Install.ps1

--- PsAppDeployToolKit/Install.ps1
+++ PsAppDeployToolKit/Install.ps1
@@ -1,16 +1,17 @@
-function TranscriptVariables{
+function TranscriptVariables() {
     $date = get-date -format "dddd-MM-dd-HH"
     $module = "psappdeploytoolkit"
     $logPath = "C:\ProgramData\Microsoft\IntuneManagementExtension\Logs\install_$module_Powershell_Module_$date.log"
-}
+    }
+    
 function Dependencies {
     if (!(Get-PackageProvider -Name NuGet -ListAvailable -ErrorAction SilentlyContinue)) {
         Write-Host "NuGet package provider is not installed. Installing..." -ForegroundColor Yellow
+        Install-PackageProvider -Name NuGet -Force
         Write-Host "NuGet package provider installed successfully." -ForegroundColor Green
     } else {
         Write-Host "NuGet package provider is already installed." -ForegroundColor Green
     }
-}
 
 TranscriptVariables
 
