937b1183f24c17440ade6040150857fd0997881d
almeida.gustavo@hotmail.fr <almeida.gustavo@hotmail.fr>
2025-02-13 16:01:00 +0100

    Adding stop-transcript before exits

--- New Teams/uninstall.ps1
+++ New Teams/uninstall.ps1
@@ -1,18 +1,19 @@
 # Function to generate log file path with current date and module name
-function Start-Logs() {
+function Start-Logs {
     param(
         [string]$AppName = "",
         [string]$Method = ""
     )
-    $date = get-date -format "dddd-MM-dd-HH"
-    $logPath = "C:\ProgramData\Microsoft\IntuneManagementExtension\Logs\$method-$AppName-$date.log"
+    $date = Get-Date -Format "dddd-MM-dd-HH"
+    $logPath = "C:\ProgramData\Microsoft\IntuneManagementExtension\Logs\$Method-$AppName-$date.log"
     Start-Transcript -Path $logPath -Append -Force
 }
-#Generate Log files
-start-logs -AppName "New Microsoft Teams" -Method "uninstall"
+
+# Generate Log files
+Start-Logs -AppName "New Microsoft Teams" -Method "uninstall"
 
 # Run the uninstallation process for Teams
-start-process -FilePath .\teamsbootstrapper.exe -ArgumentList "-x"
+Start-Process -FilePath ".\teamsbootstrapper.exe" -ArgumentList "-x" -Wait
 
 # Stop logging
 Stop-Transcript
